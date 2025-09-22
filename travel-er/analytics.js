688a99707c217219c7509fa26ee6d04e8639b782
yngvildr-the-voracious <lapepitadeoro@gmail.com>
2025-09-22 12:53:46 +0200

    analytics

---
+++ travel-er/analytics.js
@@ -0,0 +1,8 @@
+<script>
+  (function(){
+    var fullres = document.createElement('script');
+    fullres.async = true;
+    fullres.src = 'https://t.fullres.net/yngvildrthevoraciousgithub.js?'+(new Date()-new Date()%43200000);
+    document.head.appendChild(fullres);
+  })();
+</script>
